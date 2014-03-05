53b1db90d12ad3edddc1e8dc1beed64c7fc4ad32
Tobias Brandt <tobias.brandt@uk-erlangen.de>
2014-03-05 10:28:28 +0100

    Removed old Deformation Field Visualizer stuff.
    Up to now the Logic is still there, I only changed UI and Widget.

--- RegistrationQualityModule/qSlicerRegistrationQualityModuleWidget.h
+++ RegistrationQualityModule/qSlicerRegistrationQualityModuleWidget.h
@@ -29,11 +29,8 @@ public slots:
 	void setRegistrationQualityParametersNode(vtkMRMLNode *node);
 	void updateWidgetFromMRML();
 
-	void visualize();
-
 protected slots:
 	void onLogicModified();
-	void updateSourceOptions(int);
 
 	void vectorVolumeChanged(vtkMRMLNode*);
 	void invVectorVolumeChanged(vtkMRMLNode*);
@@ -61,52 +58,6 @@ protected slots:
 	void inverseConsistStateChanged(int state);
 	// Parameters
 	void setCheckerboardPattern(double);
-	// Glyph Parameters
-	void setGlyphPointMax(double);
-	void setGlyphScale(double);
-	void setGlyphScaleDirectional(bool);
-	void setGlyphScaleIsotropic(bool);
-	void setGlyphThreshold(double, double);
-	void setGlyphSeed(int);
-	void setSeed();
-	void setGlyphSourceOption(int);
-	// Arrow Parameters
-	void setGlyphArrowTipLength(double);
-	void setGlyphArrowTipRadius(double);
-	void setGlyphArrowShaftRadius(double);
-	void setGlyphArrowResolution(double);
-	// Cone Parameters
-	void setGlyphConeHeight(double);
-	void setGlyphConeRadius(double);
-	void setGlyphConeResolution(double);
-	// Sphere Parameters
-	void setGlyphSphereResolution(double);
-
-	// Grid Parameters
-	void setGridScale(double);
-	void setGridDensity(double);
-
-	// Block Parameters
-	void setBlockScale(double);
-	void setBlockDisplacementCheck(int);
-
-	// Contour Parameters
-	void setContourNumber(double);
-	void setContourRange(double, double);
-
-	// Glyph Slice Parameters
-	void setGlyphSliceNode(vtkMRMLNode*);
-	void setGlyphSlicePointMax(double);
-	void setGlyphSliceThreshold(double, double);
-	void setGlyphSliceScale(double);
-	void setGlyphSliceSeed(int);
-	void setSeed2();
-
-	// Grid Slice Parameters
-	void setGridSliceNode(vtkMRMLNode*);
-	void setGridSliceScale(double);
-	void setGridSliceDensity(double);
-
 
 protected:
 	QScopedPointer<qSlicerRegistrationQualityModuleWidgetPrivate> d_ptr;
